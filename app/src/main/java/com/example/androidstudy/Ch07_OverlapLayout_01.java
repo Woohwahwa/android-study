552efabaeba95c9b98304782f11b036ea7df4c60
Cindy <1233wsh@gmail.com>
2019-05-20 23:06:21 +0900

    ~ch_07 OverlapLayout 01

---
+++ app/src/main/java/com/example/androidstudy/Ch07_OverlapLayout_01.java
@@ -0,0 +1,13 @@
+package com.example.androidstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch07_OverlapLayout_01 extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_ch07__overlap_layout_01);
+  }
+}
